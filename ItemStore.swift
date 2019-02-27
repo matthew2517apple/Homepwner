aeda77a11e31b805209157fe3fd9abf0c231cdc3
matthew <matthew@matthew.com>
2019-02-26 19:40:33 -0600

    Chapter 11 complete (NOT including challenges)

--- ItemStore.swift
+++ ItemStore.swift
@@ -17,9 +17,25 @@ class ItemStore {
         return newItem
     }
     
-    init() {
-        for _ in 0..<5 {
-            createItem()
+    //init() {
+    //    for _ in 0..<5 {
+    //        createItem()
+    //    }
+    //}
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        let movedItem = allItems[fromIndex]
+        allItems.remove(at: fromIndex)
+        allItems.insert(movedItem, at: toIndex)
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
         }
     }
 }
--- ItemsViewController.swift
+++ ItemsViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
+    
     var itemStore: ItemStore!
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        let newItem = itemStore.createItem()
+        
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        if isEditing {
+            setEditing(false, animated: true)
+            // Inform user of available action:
+            sender.setTitle("Edit", for: .normal)
+        } else {
+            setEditing(true, animated: true)
+            // Inform user of available action:
+            sender.setTitle("Done", for: .normal)
+        }
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
@@ -35,5 +59,40 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let alertTitle = "Delete \(item.name)"
+            let alertMessage = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: {(action) -> Void in
+                self.itemStore.removeItem(item)
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+            
+            present(ac, animated: true, completion: nil)
+            
+            // Remove from the database:
+            //itemStore.removeItem(item)
+            
+            // Remove from the table view:
+            //tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    // update the database whenever a row is moved:
+    override func tableView(_ tableView: UITableView,
+                            moveRowAt sourceIndexPath: IndexPath,
+                            to destinationIndexPath: IndexPath) {
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
     }
 }
